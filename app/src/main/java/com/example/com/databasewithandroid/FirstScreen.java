cffe8fc6b8d38dcb68c45069098962dbb145e2e6
TOOFACK <paulrussianvodka@gmail.com>
2018-12-21 23:42:29 +0600

    Initial commit

---
+++ app/src/main/java/com/example/com/databasewithandroid/FirstScreen.java
@@ -0,0 +1,17 @@
+package com.example.com.databasewithandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FirstScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_screen);
+    }
+
+    public void Search(View view) {
+    }
+}
